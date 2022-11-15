4553e999c25d49b1456f77104da222ab1db0a3a4
Tiago Web <tiagosmweb@gmail.com>
2022-11-15 15:50:50 -0400

    add icone voltar

--- lib/src/auth/sign_up_screen.dart
+++ lib/src/auth/sign_up_screen.dart
@@ -14,71 +14,90 @@ class SignUpScreen extends StatelessWidget {
         child: SizedBox(
           height: size.height,
           width: size.width,
-          child: Column(
+          child: Stack(
             children: [
-              const Expanded(
-                child: Center(
-                  child: Text(
-                    "Cadastro",
-                    style: TextStyle(
+              Positioned(
+                left: 10,
+                top: 10,
+                child: SafeArea(
+                  child: IconButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    icon: const Icon(
+                      Icons.arrow_back_ios,
                       color: Colors.white,
-                      fontSize: 35,
                     ),
                   ),
                 ),
               ),
-              Container(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 32,
-                  vertical: 40,
-                ),
-                decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.vertical(
-                    top: Radius.circular(45),
+              Column(
+                children: [
+                  const Expanded(
+                    child: Center(
+                      child: Text(
+                        "Cadastro",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 35,
+                        ),
+                      ),
+                    ),
                   ),
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    const CustomTextField(
-                      icon: Icons.email,
-                      label: 'Email',
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 32,
+                      vertical: 40,
                     ),
-                    const CustomTextField(
-                      icon: Icons.lock,
-                      label: 'Senha',
-                      isSecret: true,
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.vertical(
+                        top: Radius.circular(45),
+                      ),
                     ),
-                    const CustomTextField(
-                      icon: Icons.person,
-                      label: 'Nome',
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        const CustomTextField(
+                          icon: Icons.email,
+                          label: 'Email',
+                        ),
+                        const CustomTextField(
+                          icon: Icons.lock,
+                          label: 'Senha',
+                          isSecret: true,
+                        ),
+                        const CustomTextField(
+                          icon: Icons.person,
+                          label: 'Nome',
+                        ),
+                        const CustomTextField(
+                          icon: Icons.phone,
+                          label: 'Celular',
+                        ),
+                        const CustomTextField(
+                          icon: Icons.file_copy,
+                          label: 'CPF',
+                        ),
+                        SizedBox(
+                          height: 50,
+                          child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                  shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(18),
+                              )),
+                              onPressed: () {},
+                              child: const Text(
+                                'Cadastrar usuário',
+                                style: TextStyle(
+                                  fontSize: 18,
+                                ),
+                              )),
+                        )
+                      ],
                     ),
-                    const CustomTextField(
-                      icon: Icons.phone,
-                      label: 'Celular',
-                    ),
-                    const CustomTextField(
-                      icon: Icons.file_copy,
-                      label: 'CPF',
-                    ),
-                    SizedBox(
-                      height: 50,
-                      child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                              shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(18),
-                          )),
-                          onPressed: () {},
-                          child: const Text(
-                            'Cadastrar usuário',
-                            style: TextStyle(
-                              fontSize: 18,
-                            ),
-                          )),
-                    )
-                  ],
-                ),
+                  ),
+                ],
               ),
             ],
           ),
